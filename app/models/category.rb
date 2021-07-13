b6c0c9ab1122c0090351e8b8535bb38fa974b2de
Thais Freire <thaism.freire@outlook.com>
2021-07-13 20:01:14 -0300

    more tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :bug
 
-  NAMES = ["critico", "normal"]
+  NAMES = ['critico', 'normal']
   validates :name, inclusion: { in: NAMES }
   validates :age, inclusion: { in: 1..3 }, numericality: { only_integer: true }
 end
