83cb254da8373435ad83c51f1402d6199a2cb179
Thais Freire <thaism.freire@outlook.com>
2021-07-16 17:56:16 -0300

    resolucao do bug

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,19 +10,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def today?
-    if params[:name] == 'normal' && params[:age] < 3
-      false
-    else
-      true
-  end
-
-  def todaysbugs
-    @category = Category.find(params[:id])
-    @bug = Bug.find(params[:bug_id])
-    @bugs.category.where(today: true)
-  end
-
   private
 
   def category_params
--- app/models/bug.rb
+++ app/models/bug.rb
@@ -2,4 +2,10 @@ class Bug < ApplicationRecord
   has_one :category
 
   validates :title, presence: true
+
+  def self.todaysbugs
+    Bug.all.filter do |bug|
+      bug.category.name == 'critico' || bug.category.age >= 3
+    end
+  end
 end
